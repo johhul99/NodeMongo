4724471349d6fe699e0fc1e27f5f0b48d31c97a7
johhul99 <johan.hultin@iths.se>
2025-01-20 11:39:08 +0100

    mongodb connected

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = 5250;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-console.log("Hello World!");
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my mongo api'); 
+});
+
+app.post('/blogpost', (req, res) => {
+    res.send('This is a post request');
+});
 
 
 app.listen(PORT, () => {
     console.log("Up and running on PORT 5250!");
 });
+
+mongoose.
+    connect('mongodb+srv://johanhultin99:braskmannen99@cluster0.l2ff3.mongodb.net/products-api?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+    console.log('Connected to Mongo DB');
+    });
